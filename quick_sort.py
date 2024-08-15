878582c515800fc337d0818fe3d65569e9d9e9df
Raj <raj@allsite.ai>
2024-08-15 14:40:00 +1200

    comments in quicksort

--- quick_sort.py
+++ quick_sort.py
@@ -1,13 +1,16 @@
 # Quick Sort Algorithm
 
 def Quick_Sort(arr: list):
+    # Base case if array is empty or has only one element
     if len(arr) <=1:
         return arr
     else:
+        # Selecting pivot element
         pivot = arr[0]
+        # Creating two lists to store elements smaller and larger than pivot element
         smaller = [i for i in arr[1:] if i <= pivot]
         larger = [i for i in arr[1:] if i >= pivot]
-
+        # Recursively calling Quick_Sort function on smaller and larger list
         return Quick_Sort(smaller) + [pivot] + Quick_Sort(larger)
 
 # passing data
